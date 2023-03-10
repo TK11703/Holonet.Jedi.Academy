284f7d4fba9d6f32e4d53c67337678fcce1da445
beowshawitz <beowshawitz@outlook.com>
2023-03-10 13:11:15 -0500

    Enhanced flexibility on the email senders

--- Holonet.Jedi.Academy.App/Middleware/AzCommSrvEmailSender.cs
+++ Holonet.Jedi.Academy.App/Middleware/AzCommSrvEmailSender.cs
@@ -1,6 +1,8 @@
 ﻿using Azure.Communication.Email;
+using FoolProof.Core;
 using Holonet.Jedi.Academy.Entities.Configuration;
 using Microsoft.Extensions.Options;
+using System.Net.Mail;
 
 namespace Holonet.Jedi.Academy.App.Middleware
 {
@@ -21,7 +23,8 @@ namespace Holonet.Jedi.Academy.App.Middleware
 				EmailClient emailClient = new EmailClient(config.AzCommSvcSettings.ConnectionString);
 				try
 				{
-					EmailSendOperation emailSendOperation = await emailClient.SendAsync(Azure.WaitUntil.Completed, config.AzCommSvcSettings.Sender, email, subject, htmlMessage);
+					EmailMessage message = GenerateEmailMessage(email, subject, htmlMessage);
+					EmailSendOperation emailSendOperation = await emailClient.SendAsync(Azure.WaitUntil.Completed, message);
 					EmailSendResult statusMonitor = emailSendOperation.Value;
 
 					string operationId = emailSendOperation.Id;
@@ -48,6 +51,174 @@ namespace Holonet.Jedi.Academy.App.Middleware
 			else
 			{
 				throw new Exception("Email settings were not detected.");
+			}
+		}
+
+		public async Task SendEmailAsync(List<string> emails, string subject, string htmlMessage)
+		{
+			if (config.AzCommSvcSettings != null)
+			{
+				EmailClient emailClient = new EmailClient(config.AzCommSvcSettings.ConnectionString);
+				try
+				{
+					EmailMessage message = GenerateEmailMessage(emails, subject, htmlMessage);
+					EmailSendOperation emailSendOperation = await emailClient.SendAsync(Azure.WaitUntil.Completed, message);
+					EmailSendResult statusMonitor = emailSendOperation.Value;
+
+					string operationId = emailSendOperation.Id;
+					var emailSendStatus = statusMonitor.Status;
+
+					if (emailSendStatus == EmailSendStatus.Succeeded)
+					{
+						Console.WriteLine($"Email send operation succeeded with OperationId = {operationId}.\nEmail is out for delivery.");
+					}
+					else
+					{
+						var error = statusMonitor.Error;
+						Console.WriteLine($"Failed to send email.\n OperationId = {operationId}.\n Status = {emailSendStatus}.");
+						Console.WriteLine($"Error Code = {error.Code}, Message = {error.Message}");
+						return;
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error in sending email, {ex}");
+					throw new Exception("Email settings were not detected.", ex);
+				}
+			}
+			else
+			{
+				throw new Exception("Email settings were not detected.");
+			}
+		}
+
+		public async Task SendEmailAsync(List<string> to, List<string> cc, List<string> bcc, string subject, string htmlMessage)
+		{
+			if (config.AzCommSvcSettings != null)
+			{
+				EmailClient emailClient = new EmailClient(config.AzCommSvcSettings.ConnectionString);
+				try
+				{
+					EmailMessage message = GenerateEmailMessage(to, cc, bcc, subject, htmlMessage);
+					EmailSendOperation emailSendOperation = await emailClient.SendAsync(Azure.WaitUntil.Completed, message);
+					EmailSendResult statusMonitor = emailSendOperation.Value;
+
+					string operationId = emailSendOperation.Id;
+					var emailSendStatus = statusMonitor.Status;
+
+					if (emailSendStatus == EmailSendStatus.Succeeded)
+					{
+						Console.WriteLine($"Email send operation succeeded with OperationId = {operationId}.\nEmail is out for delivery.");
+					}
+					else
+					{
+						var error = statusMonitor.Error;
+						Console.WriteLine($"Failed to send email.\n OperationId = {operationId}.\n Status = {emailSendStatus}.");
+						Console.WriteLine($"Error Code = {error.Code}, Message = {error.Message}");
+						return;
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error in sending email, {ex}");
+					throw new Exception("Email settings were not detected.", ex);
+				}
+			}
+			else
+			{
+				throw new Exception("Email settings were not detected.");
+			}
+		}
+
+		private EmailMessage GenerateEmailMessage(string emailTo, string subject, string htmlMessage)
+		{
+			EmailRecipients emailRecipients = GenerateRecipients(emailTo);
+			EmailContent content = GenerateContents(subject, htmlMessage);
+			EmailMessage message = new EmailMessage(config.AzCommSvcSettings.Sender, emailRecipients, content);
+			return message;
+		}
+
+		private EmailMessage GenerateEmailMessage(List<string> emails, string subject, string htmlMessage)
+		{
+			EmailRecipients emailRecipients = GenerateRecipients(emails);
+			EmailContent content = GenerateContents(subject, htmlMessage);
+			EmailMessage message = new EmailMessage(config.AzCommSvcSettings.Sender, emailRecipients, content);
+			return message;
+		}
+
+		private EmailMessage GenerateEmailMessage(List<string> to, List<string> cc, List<string> bcc, string subject, string htmlMessage)
+		{
+			EmailRecipients emailRecipients = GenerateRecipients(to, cc, bcc);
+			EmailContent content = GenerateContents(subject, htmlMessage);
+			EmailMessage message = new EmailMessage(config.AzCommSvcSettings.Sender, emailRecipients, content);
+			return message;
+		}
+
+		private EmailRecipients GenerateRecipients(string emailTo)
+		{
+			var toRecipients = new List<EmailAddress>();
+			if (IsValidEmail(emailTo))
+				toRecipients.Add(new EmailAddress(emailTo));
+			EmailRecipients emailRecipients = new EmailRecipients(toRecipients, null, null);
+			return emailRecipients;
+		}
+
+		private EmailRecipients GenerateRecipients(List<string> emails)
+		{
+			var toRecipients = new List<EmailAddress>();
+			var ccRecipients = new List<EmailAddress>();
+			var bccRecipients = new List<EmailAddress>();
+			foreach (var email in emails)
+			{
+				if (IsValidEmail(email))
+					bccRecipients.Add(new EmailAddress(email));
+			}
+			EmailRecipients emailRecipients = new EmailRecipients(toRecipients, ccRecipients, bccRecipients);
+			return emailRecipients;
+		}
+
+		private EmailRecipients GenerateRecipients(List<string> to, List<string> cc, List<string> bcc)
+		{
+			var toRecipients = new List<EmailAddress>();
+			var ccRecipients = new List<EmailAddress>();
+			var bccRecipients = new List<EmailAddress>();
+			foreach (var email in to)
+			{
+				if (IsValidEmail(email))
+					toRecipients.Add(new EmailAddress(email));
+			}
+			foreach (var email in cc)
+			{
+				if (IsValidEmail(email))
+					ccRecipients.Add(new EmailAddress(email));
+			}
+			foreach (var email in bcc)
+			{
+				if (IsValidEmail(email))
+					bccRecipients.Add(new EmailAddress(email));
+			}
+
+			EmailRecipients emailRecipients = new EmailRecipients(toRecipients, ccRecipients, bccRecipients);
+			return emailRecipients;
+		}
+
+		private EmailContent GenerateContents(string subject, string htmlMessage)
+		{
+			EmailContent content = new EmailContent(subject);
+			content.Html = htmlMessage;
+			return content;
+		}
+
+		private bool IsValidEmail(string emailaddress)
+		{
+			try
+			{
+				MailAddress m = new MailAddress(emailaddress);
+				return true;
+			}
+			catch (FormatException)
+			{
+				return false;
 			}
 		}
 	}
--- Holonet.Jedi.Academy.App/Middleware/SmtpEmailSender.cs
+++ Holonet.Jedi.Academy.App/Middleware/SmtpEmailSender.cs
@@ -1,4 +1,5 @@
-﻿using Holonet.Jedi.Academy.Entities.Configuration;
+﻿using FoolProof.Core;
+using Holonet.Jedi.Academy.Entities.Configuration;
 using Microsoft.Extensions.Options;
 using System.Net;
 using System.Net.Mail;
@@ -31,12 +32,134 @@ namespace Holonet.Jedi.Academy.App.Middleware
 				{
 					client.Credentials = new NetworkCredential(config.MailSettings.Sender, config.MailSettings.SenderPassword);
 				}
-				
-				return client.SendMailAsync(config.MailSettings.Sender, email, subject, htmlMessage);
+
+				MailMessage message = GenerateMailMessage(email, subject, htmlMessage);
+				return client.SendMailAsync(message);
 			}
 			else
 			{
 				throw new Exception("Email settings were not detected.");
+			}
+		}
+
+		public Task SendEmailAsync(List<string> emails, string subject, string htmlMessage)
+		{
+			if (config.MailSettings != null)
+			{
+				SmtpClient client = new SmtpClient
+				{
+					Port = config.MailSettings.Port,
+					Host = config.MailSettings.Host,
+					EnableSsl = config.MailSettings.EnableSsl,
+					DeliveryMethod = SmtpDeliveryMethod.Network,
+					UseDefaultCredentials = config.MailSettings.UseDefaultCredentials
+				};
+				if (!config.MailSettings.UseDefaultCredentials)
+				{
+					client.Credentials = new NetworkCredential(config.MailSettings.Sender, config.MailSettings.SenderPassword);
+				}
+
+				MailMessage message = GenerateMailMessage(emails, subject, htmlMessage);
+				return client.SendMailAsync(message);
+			}
+			else
+			{
+				throw new Exception("Email settings were not detected.");
+			}
+		}
+
+		public Task SendEmailAsync(List<string> to, List<string> cc, List<string> bcc, string subject, string htmlMessage)
+		{
+			if (config.MailSettings != null)
+			{
+				SmtpClient client = new SmtpClient
+				{
+					Port = config.MailSettings.Port,
+					Host = config.MailSettings.Host,
+					EnableSsl = config.MailSettings.EnableSsl,
+					DeliveryMethod = SmtpDeliveryMethod.Network,
+					UseDefaultCredentials = config.MailSettings.UseDefaultCredentials
+				};
+				if (!config.MailSettings.UseDefaultCredentials)
+				{
+					client.Credentials = new NetworkCredential(config.MailSettings.Sender, config.MailSettings.SenderPassword);
+				}
+
+				MailMessage message = GenerateMailMessage(to, cc, bcc, subject, htmlMessage);
+				return client.SendMailAsync(message);
+			}
+			else
+			{
+				throw new Exception("Email settings were not detected.");
+			}
+		}
+
+		private MailMessage GenerateMailMessage(string emailTo, string subject, string htmlMessage)
+		{
+			MailMessage message = new MailMessage(config.MailSettings.Sender, emailTo);
+			message.Subject = subject;
+			message.Body = htmlMessage;
+			return message;
+		}
+
+		private MailMessage GenerateMailMessage(List<string> emails, string subject, string htmlMessage)
+		{
+			MailMessage message = new MailMessage();
+			message.From = new MailAddress(config.MailSettings.Sender);
+			message.Subject = subject;
+			message.Body = htmlMessage;
+			AssignRecipients(message, emails);
+			return message;
+		}
+
+		private MailMessage GenerateMailMessage(List<string> to, List<string> cc, List<string> bcc, string subject, string htmlMessage)
+		{
+			MailMessage message = new MailMessage();
+			message.From = new MailAddress(config.MailSettings.Sender);
+			message.Subject = subject;
+			message.Body = htmlMessage;
+			AssignRecipients(message, to, cc, bcc);
+			return message;
+		}
+
+		private void AssignRecipients(MailMessage message, List<string> emails)
+		{
+			foreach (var email in emails)
+			{
+				if (IsValidEmail(email))
+					message.Bcc.Add(email);
+			}
+		}
+
+		private void AssignRecipients(MailMessage message, List<string> to, List<string> cc, List<string> bcc)
+		{
+			foreach (var email in to)
+			{
+				if (IsValidEmail(email))
+					message.To.Add(email);
+			}
+			foreach (var email in cc)
+			{
+				if (IsValidEmail(email))
+					message.CC.Add(email);
+			}
+			foreach (var email in bcc)
+			{
+				if (IsValidEmail(email))
+					message.Bcc.Add(email);
+			}
+		}
+
+		private bool IsValidEmail(string emailaddress)
+		{
+			try
+			{
+				MailAddress m = new MailAddress(emailaddress);
+				return true;
+			}
+			catch (FormatException)
+			{
+				return false;
 			}
 		}
 	}
